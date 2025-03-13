c905aadf3cee5aae22c5b92db6f8b85e0e27abe4
Mehdi Najmi <mehdinajmi@yahoo.com.au>
2025-03-14 06:37:46 +1100

    adding all changes after creating necessary test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignUpTest( TestCase):
+    def test_signup_page_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code,200)
+
+    def test_signup_page_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code,200)
+
+    def test_signup_form(self):
+        user=get_user_model().objects.create_user('myUesrname','myUsername@gmail.com')
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'myUesrname' )
+        self.assertEqual(get_user_model().objects.all()[0].email, 'myUsername@gmail.com')
+
+
