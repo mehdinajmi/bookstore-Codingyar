2357fe6e67368459475a68fb697f0b13ff699955
Mehdi Najmi <mehdinajmi@yahoo.com.au>
2025-03-21 07:41:28 +1100

    add media config to project

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # email Config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-#static File Config
+# static File Config
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
